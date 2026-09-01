7308b75feaa44a70e54c7f173c01b4d0c997d27b
hugo <hugo.strid@elev.ga.ntig.se>
2026-09-01 12:41:42 +0200

    first

---
+++ Lucktext/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Vad heter du?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hej {name}, vad är ditt favoritspel?");
+
+string game = Console.ReadLine();
+
+Console.WriteLine($"{game} hm... ok, ok. varför gillar du spelet?");
+
+string reason = Console.ReadLine();
+
+Console.WriteLine($"{reason}, huh. hur många år skulle du vilja spela det?");
+
+string years = Console.ReadLine();
+
+Console.WriteLine($"{years}, du ser ut som det. vem är din idol?");
+
+String idol = Console.ReadLine();
+
+Console.WriteLine("är du redo?");
+
+Console.ReadLine();
+
+Console.WriteLine($"{name} {game} {}");
+
+Console.ReadLine();
